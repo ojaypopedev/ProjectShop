23447bff95f0a903efc68359720e451aebb6cfb9
Oliver <ojaypopedev@gmail.com>
2019-11-28 03:20:57 +0000

    Honestly not quite sure
    :O

---
+++ Project_Eve/Assets/ShowPhone.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowPhone : MonoBehaviour
+{
+
+
+    [SerializeField] Vector3 onPos;
+    [SerializeField] Vector3 offPos;
+
+    bool onScreen = false;
+
+    void Update()
+    {
+
+        float mouseScroll = Input.mouseScrollDelta.y;
+
+       
+            if (mouseScroll > 1)
+            {
+                onScreen = true;
+            }
+            if (mouseScroll < -1)
+            {
+                onScreen = false;
+            }
+        
+      
+
+        transform.localPosition = Vector3.Lerp (transform.localPosition, onScreen ? (onPos) : (offPos), 4 * Time.deltaTime);
+        
+    }
+}
