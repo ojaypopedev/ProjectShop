8185d08b478b7747df9425db310b1e4a5b2f0449
BeardyKing <39307332+BeardyKing@users.noreply.github.com>
2019-11-28 00:51:25 +0000

    added do you still love me
    </3

---
+++ Project_Eve/Assets/Do you still love me/messageController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class messageController : MonoBehaviour{
+    public GameObject wife_msg_prefab;
+    public GameObject you_msg_prefab;
+
+    [SerializeField] GameObject msgContainer;
+
+    [SerializeField] Transform spawnPos;
+    [SerializeField] List<GameObject> messages;
+    [SerializeField] GameObject[] targetpos;
+
+    [SerializeField] GameObject offScreenTarget;
+    GameObject offScreenMsg;
+    [SerializeField] float speed;
+
+    int c;
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.Space)){
+            c++;
+            int x = c % 2;
+            Debug.Log(x);
+            bool isWife = x != 0;
+            AddMsg("DO YOU STILL LOVE ME?", c, isWife);
+        }
+        if (messages.Count >= 5){
+            if (offScreenMsg){
+                Destroy(offScreenMsg);
+            }
+            offScreenMsg = messages[0];
+            messages.RemoveAt(0);
+        }
+        for (int i = 0; i < messages.Count; i++){
+            messages[i].transform.position = Vector3.Lerp(messages[i].transform.position, targetpos[i].transform.position, speed * Time.deltaTime);
+        }
+        if (offScreenMsg){
+            offScreenMsg.transform.position = Vector3.Lerp(offScreenMsg.transform.position, offScreenTarget.transform.position, speed * Time.deltaTime);
+        }
+    }
+
+    void AddMsg(string inputText, int c, bool isWifesText){
+        GameObject temp;
+        if (isWifesText){
+             temp = Instantiate(wife_msg_prefab);
+        }
+        else{
+            temp = Instantiate(you_msg_prefab);
+        }
+        temp.transform.parent = msgContainer.transform;
+        temp.transform.position = spawnPos.position;
+        temp.GetComponentInChildren<Text>().text = inputText + " " + c.ToString(); ;
+        messages.Add(temp);
+    }
+}
