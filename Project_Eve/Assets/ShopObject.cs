eea9ce9df96fb4c00f426f907e6a2af2fbf9b85e
Oliver <ojaypopedev@gmail.com>
2019-11-29 19:45:01 +0000

    Tried to import the requestsa
    Need to fix

--- Project_Eve/Assets/ShopObject.cs
+++ Project_Eve/Assets/ShopObject.cs
@@ -40,14 +40,7 @@ namespace Shop
 
     }
 
-    [CreateAssetMenu()]
-    public class ShopObject : ScriptableObject
-    {
-        public string Name;    //Not used in code just for Identification.  
-        public ShopTag[] Tags; //for comparison to shop requests.
-     
-    }
-
+  
 
 
    /* [CreateAssetMenu()]
---
+++ Project_Eve/Assets/ShopTag.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Shop;
+
+[CreateAssetMenu()]
+public class ShopObject : ScriptableObject
+{
+    public string Name;    //Not used in code just for Identification.  
+    public ShopTag[] Tags; //for comparison to shop requests.
+
+}
+
